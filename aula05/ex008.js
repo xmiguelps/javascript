53aa62f05b184fe54515e7d55c2e189210f70521
Miguel Passos <182550477+xmiguelps@users.noreply.github.com>
2024-11-07 22:01:58 -0300

    Condições

---
+++ aula05/ex008.js
@@ -0,0 +1,6 @@
+let vel = 78.2
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if (vel > 60) {
+    console.log(`Voce ultrapassou a velocidade peritida. MULTADO!`)
+}
+console.log(`Dirija sempre usando cinto de segurança`)
---
+++ aula05/ex009.js
@@ -0,0 +1,7 @@
+let pais = 'Brasil'
+console.log(`Vivendo em ${pais}`)
+if (pais = 'Brasil') {
+    console.log('Brasileiro!')
+} else {
+    console.log('Estrangeiro!')
+}
