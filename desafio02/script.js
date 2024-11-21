88cefe643e8b162d10c63c4df539966eefba64f9
Miguel Passos <182550477+xmiguelps@users.noreply.github.com>
2024-11-21 16:28:14 -0300

    site pronto mas precisa arrumar

--- desafio02/script.js
+++ desafio02/script.js
@@ -7,37 +7,62 @@ function verificar() {
     let male = document.getElementById('male')
     let female = document.getElementById('female')
     let res = document.getElementById('res')
-    if (string_ano.length < 4 || (male.checked == false && female.checked == false)) {
+    let idade = date - year
+    res.style.textAlign = 'center'
+    var img = document.createElement('img')
+    img.setAttribute('id', 'foto')
+    if (string_ano.length < 4 || string_ano.length > 4 || (male.checked == false && female.checked == false)) {
         alert('[ERRO] REVISE O FORMULARIO E TENTE NOVAMENTE')
-    } else if (male.checked == true && year >= date - 5) {
-        res.innerHTML = `Detectamos Bebe de ${date - year} anos`
+    } else if (male.checked == true && idade < 5) {
+        res.innerHTML = `Detectamos Bebe de ${idade} anos`
+        img.setAttribute('src', 'bebe-masculino.jpg')
         //foto de bebe masculino
-    } else if (male.checked == true && year >= date - 12) {
-        res.innerHTML = `Detectamos Menino de ${date - year} anos`
+    } else if (male.checked == true && idade < 12) {
+        res.innerHTML = `Detectamos Menino de ${idade} anos`
+        img.setAttribute('src', 'criança-masculino.jpg')
         //foto de criança masculina
-    } else if (male.checked == true && year >= date - 15) {
-        res.innerHTML = `Detectamos Adolescente de ${date - year} anos`
+    } else if (male.checked == true && idade < 17) {
+        res.innerHTML = `Detectamos Adolescente de ${idade} anos`
+        img.setAttribute('src', 'adolescente-masculino.jpg')
         //foto de adolescente masculino
-    } else if (male.ckecked == true && year >= date - 21) {
-        res.innerHTML = `Detectamos Homem de ${date - year} anos` 
+    } else if (male.checked == true && idade < 56) {
+        res.innerHTML = `Detectamos Homem de ${idade} anos`
+        img.setAttribute('src', 'adulto-masculino.jpg') 
         //foto de adulto masculino
-    } else if (male.checkd == true && year >= date - 56) {
-        res.innerHTML = `Detectamos Idoso de ${date - year} anos`
+    } else if (male.checked == true && idade < 100) {
+        res.innerHTML = `Detectamos Idoso de ${idade} anos`
+        img.setAttribute('src', 'idoso-masculino.jpg')
         //foto de idoso masculino
-    } else if (female.checked == true && year >= date - 5) {
-        res.innerHTML = `Detectamos Bebe de ${date - year} anos`
+    } else if (female.checked == true && idade < 5) {
+        res.innerHTML = `Detectamos Bebe de ${idade} anos`
+        img.setAttribute('src', 'bebe-feminino.jpg')
         //foto de bebe feminino
-    } else if (female.checked == true && year >= date - 12) {
-        res.innerHTML = `Detectamos Menina de ${date - year} anos`
+    } else if (female.checked == true && idade < 12) {
+        res.innerHTML = `Detectamos Menina de ${idade} anos`
+        img.setAttribute('src', 'criança-feminino.jpg')
         //foto de criança feminina
-    } else if (female.checked == true && year >= date - 15) {
-        res.innerHTML = `Detectamos Adolescente de ${date - year} anos`
+    } else if (female.checked == true && idade < 17) {
+        res.innerHTML = `Detectamos Adolescente de ${idade} anos`
+        img.setAttribute('src', 'adolescente-feminino.jpg')
         //foto de adolescente feminino
-    } else if (female.checked == true && year >= date - 21) {
-        res.innerHTML = `Detectamos Mulher de ${date - year} anos`
+    } else if (female.checked == true && idade < 56) {
+        res.innerHTML = `Detectamos Mulher de ${idade} anos`
+        img.setAttribute('src', 'adulto-feminino.jpg')
         //foto de adulto feminino
-    } else if (female.checked == true && year >= date - 56) {
-        res.innerHTML = `Detectamos Idosa de ${date - year} anos`
+    } else if (female.checked == true && idade < 100) {
+        res.innerHTML = `Detectamos Idosa de ${idade} anos`
+        img.setAttribute('src', 'idoso-feminino.jpg')
         //foto de idoso feminino
     }
+    res.appendChild(img)
 }
+
+/*
+    para criar uma tag usando JavaScript, crie uma variavel e use "document.createElement('elemento que quer criar')"
+
+    voce pode colocar atributos nele fazendo:
+        variavel.setAttribute('atributo', 'oque esse atributo recebe')
+
+    e para adicionalo a algum lugar/tag faça:
+        variavel do lugar.appendChild(variavel da tag criada )
+*/
