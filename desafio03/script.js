f82a62b621c6d5d6439248adf43f5a39b275c4cf
Miguel Passos <182550477+xmiguelps@users.noreply.github.com>
2024-11-27 20:20:43 -0300

    desafio 03 completo

--- desafio03/script.js
+++ desafio03/script.js
@@ -5,8 +5,21 @@ function contar() {
     let r = document.getElementById('res')
     var fim = Number(f.value)
     var passo = Number(p.value)
-    for (var inicio = Number(i.value); inicio <= fim; inicio += passo) {
+    if (String(i.value).length == 0 || String(f.value).length == 0) {
+        r.innerText = 'Impossivel Contar'
+    } else {
+        if (String(p.value).length == 0) {
+            alert('Passo invalido! Considerando PASSO 1')
+        }
+        passo = 1
+        r.innerText = ''
+        for (var inicio = Number(i.value); inicio <= fim; inicio += passo) {
+            r.innerText += `${inicio}👉`
+        }
+        r.innerText += `🏁`
+    }
+    /*for (var inicio = Number(i.value); inicio <= fim; inicio += passo) {
         r.innerText += `${inicio}👉`
     }
-    r.innerText += `🏁`
+    r.innerText += `🏁`*/
 }
