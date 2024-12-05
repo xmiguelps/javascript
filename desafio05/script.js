6abc254554af44713bb086c1dee63b06337f86c5
Miguel Passos <182550477+xmiguelps@users.noreply.github.com>
2024-12-05 08:30:28 -0300

    principal part completed

--- desafio05/script.js
+++ desafio05/script.js
@@ -1,8 +1,8 @@
 let n = document.getElementById('inum')
 let valores = []
+const res = document.getElementById('res')
 function add() {
     let num = Number(n.value)
-    const res = document.getElementById('res')
     res.innerHTML = ''
     var opt = document.createElement('option')
     let lista = document.querySelector('select#lista')
@@ -11,6 +11,33 @@ function add() {
     valores.push(Number(n.value))
 }
 
+function maior(x) {
+    valores.sort()
+    return valores[valores.length - x]
+}
+
+function menor(n) {
+    valores.sort()
+    return valores[n]
+}
+
+function soma(c) {
+    var r = valores[0]
+    while (c < valores.length) {
+        r += valores[c]
+        c++
+    }
+    return r
+}
+
 function calcular() {
-     
+    res.innerHTML = `<p>Ao todo, temos ${valores.length} numeros cadastrados</p>`
+    
+    res.innerHTML += `<p> O maior valor informado foi ${maior(1)} </p>`
+    
+    res.innerHTML += `<p> O menor valor informado foi ${menor(0)} </p>`
+
+    res.innerHTML += `<p>Somando todos os valores, temos ${soma(1)}</p>`
+
+    res.innerHTML += `<p>A media dos valores digitados é ${soma(1) / valores.length} </p>`
 }
