e02f66698b3aed30981ad0d0d3f8ed95ac10e1f4
Miguel Passos <182550477+xmiguelps@users.noreply.github.com>
2024-12-05 11:46:29 -0300

    site completo

--- desafio05/script.js
+++ desafio05/script.js
@@ -1,14 +1,19 @@
 let n = document.getElementById('inum')
+var nstring = String(n.value)
 let valores = []
 const res = document.getElementById('res')
 function add() {
     let num = Number(n.value)
     res.innerHTML = ''
-    var opt = document.createElement('option')
-    let lista = document.querySelector('select#lista')
-    opt.innerHTML = `Valor ${num} adicionado`
-    lista.appendChild(opt)
-    valores.push(Number(n.value))
+    if (num > 100 || valores.indexOf(num) != -1 || num < 1) {
+        alert('Valor invalido ou ja encontrado na lista')
+    } else {
+        var opt = document.createElement('option')
+        let lista = document.querySelector('select#lista')
+        opt.innerHTML = `Valor ${num} adicionado`
+        lista.appendChild(opt)
+        valores.push(Number(n.value))
+    }
 }
 
 function maior(x) {
@@ -31,13 +36,17 @@ function soma(c) {
 }
 
 function calcular() {
-    res.innerHTML = `<p>Ao todo, temos ${valores.length} numeros cadastrados</p>`
+    if (lista.length < 2) {
+        alert(`Adicione pelo menos 2 numeros`)
+    } else {
+        res.innerHTML = `<p>Ao todo, temos ${valores.length} numeros cadastrados</p>`
     
-    res.innerHTML += `<p> O maior valor informado foi ${maior(1)} </p>`
-    
-    res.innerHTML += `<p> O menor valor informado foi ${menor(0)} </p>`
+        res.innerHTML += `<p> O maior valor informado foi ${maior(1)} </p>`
+        
+        res.innerHTML += `<p> O menor valor informado foi ${menor(0)} </p>`
 
-    res.innerHTML += `<p>Somando todos os valores, temos ${soma(1)}</p>`
+        res.innerHTML += `<p>Somando todos os valores, temos ${soma(1)}</p>`
 
-    res.innerHTML += `<p>A media dos valores digitados é ${soma(1) / valores.length} </p>`
+        res.innerHTML += `<p>A media dos valores digitados é ${soma(1) / valores.length} </p>`
+    }
 }
